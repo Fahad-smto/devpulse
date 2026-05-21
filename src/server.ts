b19ce898be5c6d4cde7ad313a7488d82e2762c80
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-21 23:19:25 +0600

    refactor: format code and add database initialization function

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,42 @@
 import express, { type Application, type Request, type Response } from 'express'
-const app :Application = express()
+const app: Application = express()
 const port = 3000
-import {Pool} from "pg"
+import { Pool } from "pg"
 
 // middleware
 app.use(express.json())
 
 // db connection
-const pool =new Pool({
-connectionString: "postgresql://neondb_owner:npg_5EXFSGobhY9j@ep-young-bar-aozmg4su-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+const pool = new Pool({
+  connectionString: "postgresql://neondb_owner:npg_5EXFSGobhY9j@ep-young-bar-aozmg4su-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 })
 
-app.get('/', (req : Request, res : Response) => {
+
+const initDb = async () => {
+  try {
+   await pool.query(`CREATE TABLE IF NOT EXISTS users (` +
+    `id SERIAL PRIMARY KEY,` +
+    `name VARCHAR(20) NOT NULL,` +
+    `email VARCHAR(255) NOT NULL UNIQUE,` +
+    `password VARCHAR(255) NOT NULL,` +
+     `role VARCHAR(20) NOT NULL,` +
+    `created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,` +`updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,` +
+
+  `);`)
+   console.log('Database initialized successfully')
+
+  } catch (err) {
+    console.error('Error initializing database:', err)
+  }
+}
+
+
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-app.post ('/',async(req:Request,res:Response)=>{
+app.post('/', async (req: Request, res: Response) => {
   console.log(req.body)
 })
 
