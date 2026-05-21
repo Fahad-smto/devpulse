217cc4c19df930e56a36efc4e955abf9c3eba613
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-21 22:17:51 +0600

    database connected

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express, { type Application, type Request, type Response } from 'express'
 const app :Application = express()
 const port = 3000
+import {Pool} from "pg"
 
+// middleware
 app.use(express.json())
 
+// db connection
+const pool =new Pool({
+connectionString: "postgresql://neondb_owner:npg_5EXFSGobhY9j@ep-young-bar-aozmg4su-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
 
 app.get('/', (req : Request, res : Response) => {
   res.send('Hello World!')
