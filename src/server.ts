83830888eec1017b26d18c98d133fb7312f7e85f
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-21 20:08:34 +0600

    middleware added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
-import express from 'express'
-const app = express()
+import express, { type Application, type Request, type Response } from 'express'
+const app :Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(express.json())
+
+
+app.get('/', (req : Request, res : Response) => {
   res.send('Hello World!')
 })
+
+app.post ('/',async(req:Request,res:Response)=>{
+  console.log(req.body)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
