82ddb3d4d147434cf73ab4e166b76a2d2fb88927
Fahad-smto <simanto.smto.prog@gmail.com>
2026-05-31 23:51:48 +0600

    added

--- src/server.ts
+++ src/server.ts
@@ -67,7 +67,7 @@ app.post('/', async (req: Request, res: Response) => {
     console.log(result.rows[0])
 
 
-    // console.log(req.body)
+    console.log(req.body)
     res.status(200).json({
       message: 'Data received successfully',
       success: true,
