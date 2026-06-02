64a922334a3421fe6a0b4167f6779defac755e60
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-02 23:42:36 +0600

    router added

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,29 @@
+const userController = try {
+
+    const { name, email, password } = req.body
+
+    const result = await pool.query(
+      `INSERT INTO users (name,email,password) VALUES ($1, $2, $3) RETURNING *`,
+      [name, email, password]
+    );
+
+    console.log(result.rows[0])
+
+
+    console.log(req.body)
+    res.status(200).json({
+      message: 'Data received successfully',
+      success: true,
+      data: {
+        name,
+        email,
+        password
+      }
+    })
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'Internal server error',
+      errors: err.stack
+    });
+  }
