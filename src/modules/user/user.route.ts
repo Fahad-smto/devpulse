7f371fdc8ec9ec2836391258272be3b5a0350afd
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-02 23:58:39 +0600

    user controller added

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,10 +1,11 @@
 import { Router, type Request, type Response } from "express";
 import { pool } from "../../db/index.js";
+import { userController } from "./user.controller.js";
 
 
 const router = Router()
 
-router.post('/', )
+router.post('/', userController. )
 
 
 
