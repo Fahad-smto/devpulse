aa4eddb01e17e51447a12b3d5cd78f6f85f58785
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-03 23:45:05 +0600

    user interface added

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -10,35 +10,35 @@ export interface IUser {
     updated_at: Date;
 }
 
-// export interface ICreateUser {
-//     name: string;
-//     email: string;
-//     password: string;
-//     role?: 'contributor' | 'maintainer';
-// }
+export interface ICreateUser {
+    name: string;
+    email: string;
+    password: string;
+    role?: 'contributor' | 'maintainer';
+}
 
-// export interface IUpdateUser {
-//     name?: string;
-//     email?: string;
-//     password?: string;
-//     role?: 'contributor' | 'maintainer';
-// }
+export interface IUpdateUser {
+    name?: string;
+    email?: string;
+    password?: string;
+    role?: 'contributor' | 'maintainer';
+}
 
-// export interface IUserResponse {
-//     id: number;
-//     name: string;
-//     email: string;
-//     role: string;
-//     created_at: Date;
-//     updated_at: Date;
-// }
+export interface IUserResponse {
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+    created_at: Date;
+    updated_at: Date;
+}
 
-// export interface ILoginRequest {
-//     email: string;
-//     password: string;
-// }
+export interface ILoginRequest {
+    email: string;
+    password: string;
+}
 
-// export interface ILoginResponse {
-//     token: string;
-//     user: IUserResponse;
-// }
+export interface ILoginResponse {
+    token: string;
+    user: IUserResponse;
+}
