6443c3b8d186236500f46091e156c96f78df5c1f
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-03 23:34:19 +0600

    user service added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,10 @@
 import type { Request, Response } from "express";
 import { pool } from "../../db/index.js";
+import { userService } from "./user.service.js";
 
 const userCreate = async (req: Request, res: Response) => {
     try {
-
+       const result = await userService.createUserIntoDB(req.body)
         res.status(200).json({
             message: 'Data received successfully',
             success: true,
