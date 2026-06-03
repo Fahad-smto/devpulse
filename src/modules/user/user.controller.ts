9e08b1995f94fe5844ad47bc870f43799f96ae5f
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-03 23:29:52 +0600

    user service added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,17 +4,6 @@ import { pool } from "../../db/index.js";
 const userCreate = async (req: Request, res: Response) => {
     try {
 
-        const { name, email, password } = req.body
-
-        const result = await pool.query(
-            `INSERT INTO users (name,email,password) VALUES ($1, $2, $3) RETURNING *`,
-            [name, email, password]
-        );
-
-        console.log(result.rows[0])
-
-
-        console.log(req.body)
         res.status(200).json({
             message: 'Data received successfully',
             success: true,
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,4 +2,15 @@
 
 const createUserIntoDB =async()=>{
 
+        const { name, email, password } = req.body
+
+        const result = await pool.query(
+            `INSERT INTO users (name,email,password) VALUES ($1, $2, $3) RETURNING *`,
+            [name, email, password]
+        );
+
+        console.log(result.rows[0])
+
+
+        console.log(req.body)
 }
