bf29b3a8aa875e62258c4a336262a4579ae14338
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-03 23:31:00 +0600

    user service added

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,7 +3,7 @@ import { pool } from "../../db/index.js";
 
 const createUserIntoDB =async(payload:any)=>{
 
-        const { name, email, password } = req.body
+        const { name, email, password } =  payload
 
         const result = await pool.query(
             `INSERT INTO users (name,email,password) VALUES ($1, $2, $3) RETURNING *`,
