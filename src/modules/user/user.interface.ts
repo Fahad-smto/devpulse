3fa95becb8ff0e2dd12fa8cd9dee7beec6b03c40
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-10 22:35:33 +0600

    code added

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -1,5 +1,14 @@
 // src/modules/user/user.interface.ts
 
+export interface IUser {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+    created_at: Date;
+    updated_at: Date;
+}
 
 export interface ICreateUser {
     name: string;
