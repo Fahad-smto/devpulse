c485b023e5fe46572c56501cd924b1623eb0d799
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-26 23:48:54 +0600

    added

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,19 @@
+// src/utils/jwt.utils.ts
+
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'your_secret_key_change_this';
+const JWT_EXPIRES_IN = '7d';
+
+export const signToken = (payload: { id: number; name: string; role: string }): string => {
+    return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+};
+
+
+export const verifyToken = (token: string): any => {
+    try {
+        return jwt.verify(token, JWT_SECRET);
+    } catch (error) {
+        return null;
+    }
+};
